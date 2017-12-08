398003d528a90142308f6ac90efa0fc60120da4b
Chonnawee Chatcherthaicharoen <chonnawee.ch@gmail.com>
2017-12-08 17:23:41 +0700

    Initial Commit

---
+++ ReminderHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReminderHelper
+//
+//  Created by Chonnawee Chatcherthaicharoen on 8/12/2560 BE.
+//  Copyright © 2560 Chonnawee Chatcherthaicharoen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
